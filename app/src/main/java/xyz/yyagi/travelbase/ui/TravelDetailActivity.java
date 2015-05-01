fc7e72203cecfb9629f922242faae1966e197290
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-02 08:18:49 +0900

    move activities to ui package

--- app/src/main/java/xyz/yyagi/travelbase/ui/TravelDetailActivity.java
+++ app/src/main/java/xyz/yyagi/travelbase/ui/TravelDetailActivity.java
@@ -1,9 +1,11 @@
-package xyz.yyagi.travelbase;
+package xyz.yyagi.travelbase.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import xyz.yyagi.travelbase.R;
 
 
 public class TravelDetailActivity extends Activity {
