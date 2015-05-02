004cc2607b2dfc45869f2a4dc06f598602881628
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-02 10:07:17 +0900

    add ChooseAccountManager

--- app/src/main/java/xyz/yyagi/travelbase/ui/LoginActivity.java
+++ app/src/main/java/xyz/yyagi/travelbase/ui/LoginActivity.java
@@ -1,20 +1,60 @@
 package xyz.yyagi.travelbase.ui;
 
+import android.accounts.AccountManager;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
 
 import xyz.yyagi.travelbase.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+    private SignInButton mSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         getActionBar().hide();
+
+        mSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+        mSignInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.google_sign_in_button:
+                Intent intent = AccountManager.get(this).newChooseAccountIntent(null, null, new String[]{"com.google"
+                        }, false, null,
+                        null, null, null);
+                startActivityForResult(intent, REQUEST_CODE_SIGN_IN);
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, TravelDetailActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(intent);
+            } else {
+                // TODO: Do nothing?
+            }
+        }
     }
 }
+
