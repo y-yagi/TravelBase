a49697b1b5586c0b9be68522e83413d7244fcfdd
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-01-03 15:26:36 +0900

    add floatingactionbutton

---
+++ app/src/main/java/xyz/yyagi/travelbase/ui/PlaceSearchActivity.java
@@ -0,0 +1,17 @@
+package xyz.yyagi.travelbase.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import xyz.yyagi.travelbase.R;
+
+public class PlaceSearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_search);
+
+    }
+
+}
