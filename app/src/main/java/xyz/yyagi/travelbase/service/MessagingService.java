f356de6cfe54e23af4d552efbbf622967e07d902
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-06-14 09:39:05 +0900

    remove needless comment

--- app/src/main/java/xyz/yyagi/travelbase/service/MessagingService.java
+++ app/src/main/java/xyz/yyagi/travelbase/service/MessagingService.java
@@ -19,10 +19,6 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
         Log.e(TAG, "From: " + remoteMessage.getFrom());
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         NotificationCompat.Builder noti = new NotificationCompat.Builder(this);
